f413fa3665be7471916fda75f462deef9340bd2a
farmerrrrr <hansungff3846@gmail.com>
2023-06-04 20:49:31 +0900

    [ADD] DB 연결 및 JSON 파싱 관련 수정 #NADA-25

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = require('./api/routes/router');
+const connectDB = require('./loader/db');
+const config = require('./config/config')
+
+connectDB();
 
 const app = express();
 
 // JSON 파싱을 위한 미들웨어
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/auth", router);
+app.use(router);
+
 
 // 서버 시작
-app.listen(3000, () => {
+app.listen(config.PORT, () => {
   console.log('서버가 시작되었습니다.');
 });
