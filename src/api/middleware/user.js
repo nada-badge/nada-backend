6b3513996015c5cfed0247ddd0298c3e2357b259
farmerrrrr <hansungff3846@gmail.com>
2023-06-17 00:52:50 +0900

    [FEAT] 유효성 검증 미들웨어 추가 #NADA-25

---
+++ src/api/middleware/user.js
@@ -0,0 +1,24 @@
+const { check } = require('express-validator');
+
+const validateSignUp = [
+    check('email').isEmail().withMessage('Invalid email'),
+    check('email').notEmpty().withMessage('Email is required'),
+    check('password').notEmpty().withMessage('Password is required'),
+    check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters'),
+    check('password').matches(/^(?=.*[A-Za-z])/).withMessage('Password must include one letter'),
+    check('password').matches(/^(?=.*\d)/).withMessage('Password must include one number'),
+    check('password').matches(/^(?=.*[!@#$%^&*])/).withMessage('Password must include one special character'),
+    check('userType').notEmpty().withMessage('userType is required'),
+    check('userName').notEmpty().withMessage('userName is required'),
+    check('phoneNumber').notEmpty().withMessage('phoneNumber is required'),
+];
+
+const validateLogin = [
+    check('email').notEmpty().withMessage('Email is required'),
+    check('password').notEmpty().withMessage('Password is required'),
+];
+
+module.exports = {
+    validateSignUp,
+    validateLogin
+};
--- src/api/routes/userRouter.js
+++ src/api/routes/userRouter.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const validator = require('../middleware/validate');
+const checker = require('../middleware/user');
 
 const router = express.Router();
 
-router.use('/signUp', userController.signUp);
-router.use('/login', userController.login);
+router.use('/signUp', [checker.validateSignUp, validator.validateRequest], userController.signUp);
+router.use('/login', [checker.validateLogin, validator.validateRequest], userController.login);
 
-// router.use('/logout', authController.logout);
+// router.use('/logout', userController.logout);
 
 module.exports = router;
