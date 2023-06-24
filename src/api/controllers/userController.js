1c9b4260b37f8d5701536842af19175f48bb35be
farmerrrrr <hansungff3846@gmail.com>
2023-06-24 16:59:10 +0900

    [FEAT] 사용자 닉네임 중복 체크 API #NADA-29

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const config = require('../../config/config')
 const { User, Profile } = require('../../models/user')
-const { check, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 async function signUp(req, res, next) {
      try {
@@ -13,7 +13,6 @@ async function signUp(req, res, next) {
             return res.send('error');
         }
         
-        //    회원가입 시 암호화된 비밀번호로 유저 객체가 정상적으로 생성이 안 되는 문제 발생 -------- issue 1
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -49,7 +48,6 @@ async function login(req, res, next) {
         }
 
         // 2. 비밀번호 확인
-
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({ message: '비밀번호가 올바르지 않습니다.' });
@@ -69,7 +67,24 @@ async function login(req, res, next) {
     }
 }
 
+async function checkUserName(req, res, next) {
+    try {
+        const { userName } = req.body;
+        const user = await User.findOne({ 'profile.userName': userName } );
+
+        if (user) {
+            return res.status(401).json({ result: 0, message: '이미 사용 중인 이름입니다.' });
+        }
+
+        res.status(200).json({ result: 1 });
+
+    } catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     signUp,
-    login
+    login,
+    checkUserName
 }
