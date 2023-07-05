8480f28bc38d9704f2f68e6da64fa95d742581d0
farmerrrrr <hansungff3846@gmail.com>
2023-07-06 00:07:08 +0900

    [FEAT] 대외활동 입력 API 구현 #NADA-28

---
+++ src/models/schedule.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const ScheduleSchema = new mongoose.Schema({
+    scheduleName: {
+        type: String,
+        required: true
+    },
+    groupId: {
+        type: String,
+    },
+    field: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    area: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String
+    },
+    startedAt: {
+        type: Date,
+        required: true
+    },
+    endedAt: {
+        type: Date,
+        required: true
+    },
+    registeredAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+const Schedule = mongoose.model('Schedule', ScheduleSchema);
+
+module.exports = {
+    Schedule
+};
