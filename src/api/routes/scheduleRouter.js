b0faa37551175dc935bc3bf0ed06810b637b6746
farmerrrrr <hansungff3846@gmail.com>
2023-07-12 19:10:52 +0900

    [FEAT] 대외활동 조회(get, list) API 구현 #NADA-28

--- src/api/routes/scheduleRouter.js
+++ src/api/routes/scheduleRouter.js
@@ -4,6 +4,7 @@ const scheduleController = require('../controllers/scheduleController');
 const router = express.Router();
 
 router.post('', scheduleController.addSchedule);
-// router.get('/list', scheduleController.addSchedule);
+router.get('', scheduleController.getSchedule);
+router.get('/list', scheduleController.listSchedule);
 
 module.exports = router;
