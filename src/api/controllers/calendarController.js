f1d51f0ec916b66c60e821d1ee72a8727d2b76e3
farmerrrrr <hansungff3846@gmail.com>
2023-09-26 22:53:16 +0900

    [FEAT] 사용자 관심활동 조회 API 구현 #NADA-51

--- src/api/controllers/calendarController.js
+++ src/api/controllers/calendarController.js
@@ -1,7 +1,7 @@
 const { Calendar } = require('../../models/calendar');
 const { User } = require('../../models/user');
 const { Activity } = require('../../models/activity');
-// const { toKST, setFunc } = require('../../common/utils/converter');
+const { toKST, setFunc } = require('../../common/utils/converter');
 
 async function addBookmark(req, res, next) {
     try {
@@ -47,6 +47,43 @@ async function addBookmark(req, res, next) {
     }
 };
 
+async function listBookmark(req, res, next) {
+    try {
+        const email = req.query.email;
+
+        const user = await User.findOne({ email });
+        if (!user || user.length == 0) {
+            return res.status(401).json({ message: '사용자 확인이 불가능합니다.' });
+        }
+
+        const calendarId = user.calendar;
+
+        if (!calendarId) {
+            return res.status(404).json({ message: '사용자의 캘린더 정보가 없습니다.' });
+        }
+
+        const calendar = await Calendar.findById(calendarId);
+        if (!calendar || calendar.length == 0) {
+            return res.status(404).json({ message: '사용자의 캘린더를 찾을 수 없습니다.' });
+        }
+
+        const activityPromises = calendar.activitys.map(async activityId => {
+            const activity = await Activity.findById(activityId);
+            return activity;
+          });
+          
+        const activitiesUTC = await Promise.all(activityPromises);
+
+        const activities = setFunc(activitiesUTC, ['registeredAt', 'updatedAt', 'startedAt', 'endedAt'], toKST);
+        
+        res.status(200).json({ activities });
+    }
+    catch (err) {
+        next(err);
+    }
+}
+
 module.exports = {
     addBookmark,
+    listBookmark
 };
