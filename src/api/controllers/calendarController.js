8859516cf153d8fd8de0f24eb834bfba2f187b94
farmerrrrr <hansungff3846@gmail.com>
2023-10-06 20:27:43 +0900

    [ADD] 월별 조회 로직 추가 #NADA-51

--- src/api/controllers/calendarController.js
+++ src/api/controllers/calendarController.js
@@ -2,6 +2,7 @@ const { Calendar } = require('../../models/calendar');
 const { User } = require('../../models/user');
 const { Activity } = require('../../models/activity');
 const { toKST, setFunc } = require('../../common/utils/converter');
+const { isWithinInterval, isSameMonth, isSameYear } = require('date-fns');
 
 async function addBookmark(req, res, next) {
     try {
@@ -54,6 +55,65 @@ async function addBookmark(req, res, next) {
 async function listBookmark(req, res, next) {
     try {
         const email = req.query.email;
+        const targetYear = req.query.year;
+        const targetMonth = req.query.month;
+        
+        if ((targetYear && !targetMonth) || (!targetYear && targetMonth)) {
+            return res.status(400).json({ message: '기준 연도와 월을 함께 입력해주세요.' });
+        }
+
+        const user = await User.findOne({ email });
+  
+        if (!user || user.length == 0) {
+            return res.status(401).json({ message: '사용자 확인이 불가능합니다.' });
+        }
+
+        const calendarId = user.calendar;
+
+        if (!calendarId) {
+            return res.status(404).json({ message: '사용자의 캘린더 정보가 없습니다.' });
+        }
+
+        const calendar = await Calendar.findById(calendarId);
+        if (!calendar || calendar.length == 0) {
+            return res.status(404).json({ message: '사용자의 캘린더를 찾을 수 없습니다.' });
+        }
+
+        let activitiesPromises = calendar.activities.map(async activityId => {
+            const activity = await Activity.findById(activityId);
+            return activity;
+        });
+        
+        let activitiesUTC = await Promise.all(activitiesPromises);
+   
+        if (targetMonth && targetYear) {
+            activitiesUTC = activitiesUTC.filter(activity => {
+                const activityStartDate = new Date(activity.startedAt);
+                const activityEndDate = new Date(activity.endedAt);
+    
+                // 시작일과 종료일 사이에 targetMonth 및 targetYear가 있는 경우 선택
+                if (
+                    (isWithinInterval(new Date(targetYear, targetMonth - 1, 1), { start: activityStartDate, end: activityEndDate }) ||
+                    isSameMonth(new Date(targetYear, targetMonth - 1, 1), activityStartDate)) &&
+                    isSameYear(new Date(targetYear, targetMonth - 1, 1), activityStartDate)
+                ) {
+                    return true;
+                }
+                return false;
+            });
+        }
+        const activities = setFunc(activitiesUTC, ['registeredAt', 'updatedAt', 'startedAt', 'endedAt'], toKST);
+        
+        res.status(200).json({ activities });
+    }
+    catch (err) {
+        next(err);
+    }
+}
+
+async function removeBookmark(req, res, next) {
+    try {
+        const email = req.query.email;
 
         const user = await User.findOne({ email });
         if (!user || user.length == 0) {
@@ -89,5 +149,6 @@ async function listBookmark(req, res, next) {
 
 module.exports = {
     addBookmark,
-    listBookmark
+    listBookmark,
+    removeBookmark
 };
