169298476c061dc949490432d17e12127534d259
farmerrrrr <hansungff3846@gmail.com>
2023-11-24 14:18:25 +0900

    [ADD] JWT 토큰 정보 추가 및 관련 코드 리팩토링 #NADA-25

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,8 +1,7 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const config = require('../../config/config')
-const { User, Profile, Group } = require('../../models/user')
+const bcrypt = require('bcryptjs');
+const { User, Profile, Group } = require('../../models/user');
 const { validationResult } = require('express-validator');
+const { generateToken } = require('../../common/utils/jwt');
 
 async function signUp(req, res, next) {
      try {
@@ -52,24 +51,17 @@ async function login(req, res, next) {
     try {
         const { email, password } = req.body;
 
-        // 1. 사용자 확인
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(401).json({ message: '사용자 확인이 불가능합니다.' });
         }
 
-        // 2. 비밀번호 확인
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(401).json({ message: '비밀번호가 올바르지 않습니다.' });
         }
 
-        // 3. 토큰 발급
-        const token = jwt.sign(
-            { email: user.email },
-            config.JWT_SECRET,
-            { expiresIn: '1h' }
-        );
+        const token = generateToken(user);
 
         res.status(200).json({ token });
         
