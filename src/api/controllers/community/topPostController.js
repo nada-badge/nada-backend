27803e085005721ed53ec7d6f12ee7f18caa3efa
KaSangeun <chloeka127@gmail.com>
2024-01-22 14:37:15 +0000

    [CHORE] 인기 게시물 선정 반환 값 title, category, imageUrl으로 변경 #NADA-72

--- src/api/controllers/community/topPostController.js
+++ src/api/controllers/community/topPostController.js
@@ -9,11 +9,11 @@ async function topPosts(req, res, next) {
 	
 	let posts;
         if (totalPostCount <= 3) {
-            posts = await Post.find().sort({ views: -1 }).limit(3).exec();
+            posts = await Post.find().sort({ views: -1 }).select('title category imageUrl').limit(3).exec();
         } else {
             posts = await Post.find({ 'registeredAt': {$gte: threeDaysAgo} })
             .sort({ views: -1, _id: 1  })
-            .select('category title imageUrl')
+            select('title category imageUrl')            
             .limit(3);
         }
 
