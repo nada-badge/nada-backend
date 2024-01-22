4022422aad1e4087f4c25888aeececd4981c527d
KaSangeun <chloeka127@gmail.com>
2024-01-22 06:26:16 +0000

    [ADD] 인기 게시물 선정 조건 추가 #NADA-72

--- src/api/controllers/community/topPostController.js
+++ src/api/controllers/community/topPostController.js
@@ -8,14 +8,14 @@ async function topPosts(req, res, next) {
         const totalPostCount = await Post.countDocuments({registeredAt: { $gte: threeDaysAgo }});
 	
 	let posts;
-        if (totalPostCount <= 5) {
-            posts = await Post.find().sort({ views: -1 }).exec();
+        if (totalPostCount <= 3) {
+            posts = await Post.find().sort({ views: -1 }).limit(3).exec();
         } if (posts.length === 0) {
             posts = null;
         } else {
             posts = await Post.find({ 'registeredAt': {$gte: threeDaysAgo} })
             .sort({ views: -1, _id: 1  })
-            .limit(5);
+            .limit(3);
         }
 
 	res.status(200).json({ posts: posts });
