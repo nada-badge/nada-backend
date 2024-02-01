7ca998ba0995895345580a87cf2adb06f7f9b02e
farmerrrrr <hansungff3846@gmail.com>
2024-02-01 14:42:55 +0900

    [FIX] 카테고리 구분 변경 #NADA-28

--- src/common/const/activity.js
+++ src/common/const/activity.js
@@ -4,7 +4,7 @@ const field = COMMUNITY.field;
 
 const region = COMMUNITY.region;
 
-const inActivity = ["공모전", "지역/연합 동아리", "교내 동아리", "소모임", "프로젝트", "대외활동", "인턴"];
+const inActivity = ["공모전", "지역 동아리", "연합 동아리", "교내 동아리", "소모임", "프로젝트", "대외활동", "인턴"];
 
 module.exports = {
     field,
