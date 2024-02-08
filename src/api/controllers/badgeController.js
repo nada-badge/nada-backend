a60c7407002cec5ea2cef86459bc61a0c701aa38
farmerrrrr <hansungff3846@gmail.com>
2024-02-08 17:28:47 +0900

    [FEAT] 뱃지 발급 API 구현 #NADA-73

--- src/api/controllers/badgeController.js
+++ src/api/controllers/badgeController.js
@@ -1,55 +1,98 @@
 const { Badge } = require('../../models/badge');
+const { Group, User } = require('../../models/user');
 const { contract } = require('../../loader/web3');
 const { generateBadgeId, call } = require('../../services/chain');
 
 async function issueBadge(req, res, next) {
     try {
-        // 하나만 발급한다고 생각하고 구현해보자!
         const reqInfo = req.body;
 
-    /*
-        const isExist = await User.findOne({ 'userType': 2, 'email': reqInfo.issuer });
+        const issuer = await User.findOne({ 'userType': 2, 'email': reqInfo.issuer });
 
-        if(isExist == null) {
+        if(!issuer) {
             return res.status(401).json({ message: '발급자 확인이 불가능합니다.' });
         }
-    */
-        const badgeInfo = {
-            id: "",
-            owner: reqInfo.ownerEmail,
-            category: reqInfo.category,
-            role: reqInfo.groupInfo.role,
-            issuer: reqInfo.issuer
-        };
+        const group = await Group.findById(issuer.groups[0]);
 
-        const badgeId = generateBadgeId(badgeInfo);
+        let issuedBadges = [];
+        let badgeInfos = [];
 
-        const transactionData = await contract.methods.claim(badgeInfo, badgeId).encodeABI();
+        await Promise.all(reqInfo.members.map(async member => {
+
+            let badgeInfo = {
+                badgeId: "",
+                id: "",
+                owner: member.email,
+                category: group.category,
+                role: member.role,
+                issuer: issuer.email
+            };
+
+            const badgeId = generateBadgeId(badgeInfo);
+            badgeInfo.badgeId = badgeId;
+
+            badgeInfos.push(badgeInfo);
+
+            const badge = new Badge({
+                badgeIdAtChain: badgeId,
+                onwerEmail: member.email,
+                badgeName: reqInfo.badgeName,
+                category: group.category,
+                groupInfo: {
+                    groupName: group.groupName,
+                    team: member.team,
+                    role: member.role
+                },
+                issuer: issuer.email,
+                description: reqInfo.description,
+                records: reqInfo.records,
+                status: "issued",
+                badgeImageUrl: reqInfo.badgeImageUrl,
+            });
+
+            await badge.save();
+
+            // 멤버가 서비스에 등록된 회원인지 확인
+            const memberUser = await User.findOne({ 'email': member.email });
+
+            // 등록된 회원이면,
+            if(memberUser) {
+                // 1) 그 회원의 그룹 정보에 해당 그룹이 있는지 확인 후 연결
+                if(!memberUser.groups.includes(group._id)) {
+                    memberUser.groups.push(group);
+                }
+                // 2) 회원 정보에 뱃지 데이터 저장 (회원으로서의 멤버 정보 업데이트)
+                memberUser.badges.push(badge._id);
+                await memberUser.save();
+                
+                member.isUser = true;
+            }
+
+            // 기존부터 그룹에 포함된 회원인지 확인
+            const memberInGroup = group.memberInfo.find(memberInfo => memberInfo.email === member.email);
+            if(!memberInGroup) {
+                // 멤버 자체의 데이터를 저장하기 위해 member 포맷의 데이터에 badge 정보 추가 후 저장
+                member.badges = [];
+                member.badges.push(badge._id);
+                group.memberInfo.push(member);
+            }
+            else {
+                memberInGroup.badges.push(badge._id);
+            }
+
+            issuedBadges.push(badge);
+        }));
+
+        const transactionData = await contract.methods.claim(badgeInfos).encodeABI();
         const receipt = await call(transactionData);
 
         if (!receipt.status) {
             return res.status(500).json({ message: '트랜잭션이 실패하였습니다.' });
         }
 
-        const badge = new Badge({
-            onwerEmail: reqInfo.onwerEmail,
-            badgeName: reqInfo.badgeName,
-            category: reqInfo.category,
-            groupInfo: {
-                groupName: reqInfo.groupName,
-                team: reqInfo.team,
-                role: reqInfo.role
-            },
-            issuer: reqInfo.issuer,
-            description: reqInfo.description,
-            status: "issued",
-            badgeImageUrl: reqInfo.badgeImageUrl,
-            issuedAt: Date.now
-        });
+        await group.save();
 
-//        await badge.save();
-
-        res.status(200).json({ badgeInfo });
+        res.status(200).json({ issuedBadges });
     }
     catch (err) {
         next(err);
