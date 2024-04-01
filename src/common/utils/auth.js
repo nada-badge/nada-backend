ce83cc392465716ea722c8c253d14949a080c8a0
farmerrrrr <hansungff3846@gmail.com>
2024-04-01 16:37:51 +0900

    [ADD] 리프레시 토큰 생성 및 검증 함수 구현 #NADA-91

--- src/common/utils/auth.js
+++ src/common/utils/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { RefreshToken } = require('../../models/jwt')
 const config = require("../../config/config"); 
 
 // access token 생성
@@ -21,6 +22,35 @@ function verifyAccessToken(token) {
     }
 }
 
+// refresh token 생성
+async function generateRefreshToken(userId) {
+    try {
+        const refreshToken = jwt.sign({ userId }, config.JWT_REFRESH_SECRET, { expiresIn: '14d' });
+        await RefreshToken.create({ userId, refreshToken, expiresAt });
+        await refreshToken.save()
+        
+        return refreshToken;
+    } catch {
+        return res.status(403).json({ message: 'refresh token 생성에 실패하였습니다.' });
+    }
+}
+
+// refresh token 검증
+async function verifyRefreshToken(token) {
+    try {
+        const decoded = jwt.verify(token, config.JWT_REFRESH_SECRET);
+        const storedToken = await RefreshToken.findOne({ userId: decoded.userId, token: token });
+
+        if(!storedToken || storedToken.expiredAt < new Date()) {
+            return res.status(403).json({ message: 'refresh token이 존재하지 않거나 유효하지 않습니다.' });
+        }
+        
+        return decoded;
+    } catch {
+        return res.status(403).json({ message: 'refresh token 검증에 실패하였습니다.' });
+    }
+}
+
 async function generateHashedPassword(password) {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -35,6 +65,8 @@ async function comparePassword(password1, password2) {
 module.exports = { 
     generateAccessToken,
     verifyAccessToken,
+    generateRefreshToken,
+    verifyRefreshToken,
     generateHashedPassword,
     comparePassword
 };
---
+++ src/models/jwt.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const refreshTokenSchema = new mongoose.Schema({
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    token: { 
+        type: String,
+        required: true 
+    },
+    expiredAt: { 
+        type: Date, 
+        required: true 
+    }
+});
+
+const RefreshToken = mongoose.model('RefreshToken', refreshTokenSchema);
+
+module.exports = {
+    RefreshToken
+};
